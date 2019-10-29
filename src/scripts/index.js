de00020faaa9354cb1d9ed7f3446ea085aa37132
grychanyukdo <grychanyukdo@gmail.com>
2019-10-29 22:27:10 +0300

    PWA start mode

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,33 +10,43 @@ if (window.isIOS) document.documentElement.classList.add('ios');
 window.addEventListener("load", () => {
     document.documentElement.classList.add('load');
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/public/sw.js')
+            .then(function(registration) {
+                console.log('Registration successful, scope is:', registration.scope);
+            })
+            .catch(function(error) {
+                console.log('Service worker registration failed, error:', error);
+            });
+    }
+
 
     const gra = function(min, max) {
         return Math.random() * (max - min) + min;
     }
-    
+
     const gri = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    
-    const init = function(){
+
+    const init = function() {
         let items = document.querySelectorAll('.gallery li');
-        for (let i = 0; i < items.length; i++){
-            items[i].style.minWidth = gra(20,20) + 'vw';
+        for (let i = 0; i < items.length; i++) {
+            items[i].style.minWidth = gra(20, 20) + 'vw';
             items[i].style.background = randomColor({ luminosity: 'light' });
         }
         cssScrollSnapPolyfill()
     }
     init();
 
-    
-    
-    const init2 = function(){
+
+
+    const init2 = function() {
         let items = document.querySelectorAll('.scroller li');
-        for (let i = 0; i < items.length; i++){
-            items[i].style.minHeight = gra(20,30) + 'vh';
-            items[i].style.background = randomColor({luminosity: 'light'});
-        }        
+        for (let i = 0; i < items.length; i++) {
+            items[i].style.minHeight = gra(40, 50) + 'vh';
+            items[i].style.background = randomColor({ luminosity: 'dark' });
+        }
         cssScrollSnapPolyfill()
     }
     init2();
